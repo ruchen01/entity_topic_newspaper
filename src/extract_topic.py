653cb488f53db7f5f40fb2870f30d3601d6b7836
ruchen01 <jcherry726@yahoo.com>
2019-06-17 11:49:02 -0700

    Change the format of code.

--- src/extract_topic.py
+++ src/extract_topic.py
@@ -5,18 +5,16 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 
 
-
-
-def generate_text_li(text_dir):
-    text_li = glob.glob(text_dir+'/*.txt')
+def generate_text_list(text_dir):
+"""group all articles into a list""" 
+    text_list = glob.glob(text_dir+'/*.txt')
     text_data = []
-    for file_name in text_li:
+    for file_name in text_list:
         with open(file_name) as f:
             #data = f.readlines()
             data = " ".join(line.strip() for line in f)
             text_data.append(data)
     return text_data
-
 
 
 class LemmaCountVectorizer(CountVectorizer):
@@ -27,23 +25,22 @@ class LemmaCountVectorizer(CountVectorizer):
 
 
 def process_text2vec(text):
-
-    
-    tf_vectorizer = LemmaCountVectorizer(max_df=0.95,
+    cnt_vectorizer = LemmaCountVectorizer(max_df=0.95,
                                          min_df=2,
                                          stop_words='english',
                                          decode_error='ignore')
-    tf = tf_vectorizer.fit_transform(text)
-    return tf, tf_vectorizer
+    cnt_mat = cnt_vectorizer.fit_transform(text)
+    return cnt_mat, cnt_vectorizer
 
-def model(tf, n_comp = 4):
 
+def model(cnt_mat, n_comp = 4):
     lda = LatentDirichletAllocation(n_components=n_comp, max_iter=5,
                                 learning_method = 'online',
                                 learning_offset = 50.,
                                 random_state = 0)
-    lda.fit(tf)
+    lda.fit(cnt_mat)
     return lda
+
 
 def print_top_words(model, feature_names, n_top_words=10):
     for index, topic in enumerate(model.components_):
@@ -51,7 +48,8 @@ def print_top_words(model, feature_names, n_top_words=10):
         message += " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1 :-1]])
         print(message)
         print("="*70)
-        
+
+
 def print_topic(text_dir, out_dir, n_top_words = 10):
     text = generate_text_li(text_dir)
     tf, tf_vectorizer = process_text2vec(text)
