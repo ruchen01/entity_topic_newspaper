05ba3b83ff97ce8ff1e76510176d4fc14891a3da
ruchen01 <jcherry726@yahoo.com>
2019-06-17 11:49:02 -0700

    Change the format of code.

--- src/get_ocr.py
+++ src/get_ocr.py
@@ -1,18 +1,31 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
 import boto3
 import io
 from PIL import Image
-
-s3 = boto3.resource('s3', region_name='us-west-2')
-bucket = s3.Bucket('newspaper-ru-insight')
-object = bucket.Object('13_01_000001.tif')
-
-file_stream = io.BytesIO()
-object.download_fileobj(file_stream)
-print(file_stream)
-image = Image.open(file_stream)
+import pytesseract
 
 
-#img = mpimg.imread(file_stream)
+def img2text_s3(bucket_name, text_folder='text', img_folder='img')
+"""For given img folder in s3, generate OCR'd text into the text folder"""
+    #bucket_name = 'newspaper-ru-insight'
+    s3 = boto3.resource('s3', region_name='us-west-2')
+    bucket = s3.Bucket(bucket_name)
+    max_cnt = 200
+    cnt = 0
+    for obj in bucket.objects.filter(Delimiter='/', Prefix=img_folder+'/'):
+      print(obj.key)
+      img_file = obj.key
+      object=bucket.Object(img_file)
+      if cnt < max_cnt:
+        object=bucket.Object(obj.key)
+        file_stream = io.BytesIO()
+        object.download_fileobj(file_stream)
+        #print(file_stream)
+        image = Image.open(file_stream)
+        text = pytesseract.image_to_string(image)
+        text_file = text_folder + img_file[3:-3] +'txt'
+        object=s3.Object(bucket_name, text_file)
+        object.put(Body=text)
+        cnt += 1
+      else:
+        break
+    
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,7 @@
 import sys
-
 import extract_entity
 import extract_topic
-
+import get_ocr
 
 
 def main(argv):
@@ -10,8 +9,8 @@ def main(argv):
     text_file = '../data/preprocessed/13_01_000002/1.txt'
     out_dir = '../out'
     text_dir = '../data/preprocessed/13_01_000002'
-
-    #add OCR part here
+    bucket_name ='newspaper-ru-insight' 
+    img2text_s3(bucket_name, text_folder='text', img_folder='img')
     extract_entity.export_entity_csv(text_file, out_dir)
     extract_topic.print_topic(text_dir, out_dir)
 
