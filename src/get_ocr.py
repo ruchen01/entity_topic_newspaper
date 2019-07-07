6ac2edd2e6a3b264f51e802e7bfb49795f541cb3
ruchen01 <jcherry726@yahoo.com>
2019-07-07 16:42:42 -0700

    update get_ocr

--- src/get_ocr.py
+++ src/get_ocr.py
@@ -7,7 +7,7 @@ from src.process import Newspaper
 
 def imgs_to_texts_save_local(img_dir='./data/img', output_dir ='./output/text'):
     """ Save all OCR'd text to local directory    """
-    img_files = glob.glob(img_dir + '/*.tif')
+    img_files = glob.glob(img_dir + '/*')
     print(img_files)
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
